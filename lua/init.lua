b01cabc9e693665745496dbe25de553c1b5617ec
Robert Altner <robert.altner@allegro-packets.com>
2023-07-05 14:23:13 +0200

    fix rooting

--- lua/init.lua
+++ lua/init.lua
@@ -8,9 +8,10 @@ local colors = require("tokyonight.colors").setup()
 require("editorconfig").properties.trim_trailing_whitespace = false
 
 require("nvim-tree").setup({
+	update_root = true,
 	update_focused_file = {
-		enable = false,
-		update_cwd = false
+		enable = true,
+		update_cwd = trie
 	}
 })
 require("mason").setup()
